9bb0d83960b6f84314a037d1d72f834609b1661a
DESKTOP-TIDRHG5\ruize <nalanchangge@gmail.com>
2022-04-20 11:55:36 +0200

    test

--- TPS_WebService/src/Model/DataStore.java
+++ TPS_WebService/src/Model/DataStore.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class DataStore {
 
-    private static ArrayList<Article> articles;
+    private static Notizia notizia;
 
     public static ArrayList<Article> getArticles() {
-        return articles;
+        return notizia.getArticles();
     }
 
-    public static void setArticles(ArrayList<Article> articles) {
-        DataStore.articles = articles;
+    public static void setNotizia(Notizia notizia) {
+        DataStore.notizia = notizia;
     }
 }
